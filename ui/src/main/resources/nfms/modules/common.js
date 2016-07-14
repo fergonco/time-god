6a88fafd31a57e87c15f72c4da6338d76e615b98
Fernando González Cortés <fernando.gonzalez@geomati.co>
2016-07-14 10:14:29 +0200

    developer totals

--- ui/src/main/resources/nfms/modules/common.js
+++ ui/src/main/resources/nfms/modules/common.js
@@ -34,6 +34,17 @@ define([ "message-bus" ], function(bus) {
       return minutes < 10 ? "0" + minutes : minutes;
    }
 
+   function getDeveloperTimes(developerAccum) {
+      var ret = "\n";
+      var total = 0;
+      for ( var developerName in developerAccum) {
+         ret += "* Acumulado " + developerName + ": " + Math.round(developerAccum[developerName] / 3600000) + "h\n";
+         total += developerAccum[developerName];
+      }
+      ret += "* Acumulado total: " + Math.round(total / 3600000) + "\n\n\n";
+      return ret;
+   }
+
    bus.listen("show-time-report", function(e, message) {
       var timeSegments = [];
       for (var i = 0; i < pokers.length; i++) {
@@ -58,28 +69,38 @@ define([ "message-bus" ], function(bus) {
 
       var wiki = "";
       var lastDay = null;
+      var developerAccum = {};
       for (var i = 0; i < timeSegments.length; i++) {
          var start = new Date(timeSegments[i].start);
          var end = new Date(timeSegments[i].end);
+
          if (lastDay == null || start.getDate() != lastDay.getDate() || start.getMonth() != lastDay.getMonth()
             || start.getFullYear() != lastDay.getFullYear()) {
-            if (wiki.length > 0) {
-               wiki += "\n\n";
-            }
+            wiki += getDeveloperTimes(developerAccum);
+
             wiki += start.getDate() + "/" + start.getMonth() + "/" + start.getFullYear() + "\n";
-            wiki += "-".repeat(30) + "\n";
-            
+            wiki += "-".repeat(30) + "\n\n";
+
             lastDay = start;
          }
 
+         // accumulate on developers
+         var developerName = timeSegments[i].developer.name;
+         if (!developerAccum.hasOwnProperty(developerName)) {
+            developerAccum[developerName] = 0;
+         }
+         developerAccum[developerName] += timeSegments[i].end - timeSegments[i].start;
+
+         // time segments log
          wiki += format(start.getHours()) + "." + format(start.getMinutes());
          wiki += " - ";
          wiki += format(end.getHours()) + "." + format(end.getMinutes());
-         wiki += "\t" + timeSegments[i].developer.name;
+         wiki += "\t" + developerName;
          wiki += "\t" + timeSegments[i].poker.name;
          wiki += "\t" + timeSegments[i].task.name;
          wiki += "\n";
       }
+      wiki += getDeveloperTimes(developerAccum);
 
       var w = window.open();
       w.document.open();
