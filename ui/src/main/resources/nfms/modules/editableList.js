8ae9bf8f61b400f4ce05e2a8bd6fc8f8efd8fbad
Fernando González Cortés <fernando.gonzalez@geomati.co>
2016-08-09 17:41:21 +0200

    styles

--- ui/src/main/resources/nfms/modules/editableList.js
+++ ui/src/main/resources/nfms/modules/editableList.js
@@ -1,4 +1,4 @@
-define([ "message-bus", "ui-values", "d3" ], function(bus, uiValues) {
+define([ "message-bus", "jsdialogs", "d3" ], function(bus) {
 
    function create(containerId) {
       var id = new Date().getTime();
@@ -10,11 +10,6 @@ define([ "message-bus", "ui-values", "d3" ], function(bus, uiValues) {
       var entryClassName = null;
       var postProcess = null;
 
-      bus.send("ui-input-field:create", {
-         "div" : "txtAdd-" + id,
-         "parentDiv" : containerId
-      });
-
       bus.send("ui-button:create", {
          "div" : "editableList-btn-add-" + id,
          "parentDiv" : containerId,
@@ -22,8 +17,15 @@ define([ "message-bus", "ui-values", "d3" ], function(bus, uiValues) {
          "sendEventName" : "editableList-btn-add-" + id
       });
       bus.listen("editableList-btn-add-" + id, function() {
-         add(uiValues.get("txtAdd-" + id));
-         uiValues.set("txtAdd-" + id, "");
+         bus.send("jsdialogs.question", [ {
+            "message" : "Introduce el nombre de la nueva tarea",
+            "okAction" : function(value) {
+               if (value.trim().length > 0) {
+                  add(value);
+               }
+            },
+            "initialValue" : "Nuevo " + entryClassName
+         } ]);
       });
 
       var instance = {
@@ -43,7 +45,7 @@ define([ "message-bus", "ui-values", "d3" ], function(bus, uiValues) {
             var selection = d3.select("#" + containerId).selectAll("." + entryClassName).data(list);
             selection.exit().remove();
             selection.enter().append("div");
-            selection.attr("class", entryClassName);
+            selection.attr("class", "editableListEntry " + entryClassName);
             selection.html(function(d) {
                return renderer(d);
             });
